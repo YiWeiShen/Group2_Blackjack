0f28e6a0f68a21df19ff2d71cd7ca8949bc29373
XtC4 <xutongchen0@gmail.com>
2021-08-11 21:19:54 -0400

    first commit

--- app/src/main/java/com/example/group2_blackjack/MainActivity2.java
+++ app/src/main/java/com/example/group2_blackjack/MainActivity2.java
@@ -2,15 +2,13 @@ package com.example.group2_blackjack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 }
